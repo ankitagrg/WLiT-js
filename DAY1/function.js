f0800e9966c0af83ba832011c010d9ebd4dc8183
ankitagrg <ankitalg18@gmail.com>
2024-07-27 21:38:17 +0545

    JS-basic

---
+++ DAY1/function.js
@@ -0,0 +1,21 @@
+// Regular function //
+
+function sum(a, b) {
+    return a + b;
+ }
+
+
+//  Product of two numbers //
+ let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+  return a * b;
+}
+
+
+//  Sum of two numbers //
+let y = myFunction(4, 6);
+function MyFunction(a, b) {
+    return a + b;
+    }
+
